9ab321dd43b111d045cd35ffbca11878da2eac70
Vikrant <vikrant.singh3@aubank.in>
2022-11-22 10:47:35 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-form';
+  title = 'Angular Form';
+  loginUser(item:any)
+  {
+    console.warn(item);
+  }
 }

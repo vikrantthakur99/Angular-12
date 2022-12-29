9881b10f193f0ce0b5c091c95d4557647103bc13
Vikrant <vikrant.singh3@aubank.in>
2022-12-29 15:05:39 +0530

    first commit of the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employeedetails';
+
+  constructor(private http:HttpClient){}
+
+  onEmployeeCreate(employees: {ename:string, desc:string, rank:string}){
+  console.log(employees);
+  const headers = new HttpHeaders ({'myHeader':'DevOps'});
+  this.http.post('https://angularhttp-e57c7-default-rtdb.firebaseio.com/employees.json',employees,{headers:headers})
+  .subscribe((res)=> {
+    console.log(res);
+  });
+  
+  }
 }
